bd4d71ca04458204e67aac796f93a428fb4144f2
Walt Destler <walt@waltdestler.com>
2018-11-15 12:38:05 -0800

    Start of demonstration.

--- particle.cpp
+++ particle.cpp
@@ -4,8 +4,8 @@
 void Particle::update(float dt)
 {
 	life += dt;
-	center += velocity * dt;
-	velocity += acceleration * dt;
+	//center += velocity * dt;
+	//velocity += acceleration * dt;
 }
 
 bool Particle::isDead()
@@ -17,21 +17,21 @@ void Particle::draw(RenderWindow& window)
 {
 	RectangleShape shape;
 	shape.setPosition(center);
-	//shape.setSize(Vector2f(50, 50));
-	//shape.setOrigin(25, 25);
+	shape.setSize(Vector2f(50, 50));
+	shape.setOrigin(25, 25);
 	
-	shape.setTexture(texture);
+	//shape.setTexture(texture);
 
-	float t = life / lifetime;
-	Vector2f size = lerp(startSize, endSize, t);
-	shape.setSize(size);
-	shape.setOrigin(size / 2.0f);
+	//float t = life / lifetime;
+	//Vector2f size = lerp(startSize, endSize, t);
+	//shape.setSize(size);
+	//shape.setOrigin(size / 2.0f);
 
-	ColorF color = lerp(startColor, endColor, t);
-	shape.setFillColor(color);
+	//ColorF color = lerp(startColor, endColor, t);
+	//shape.setFillColor(color);
 
-	float rot = lerp(startRot, endRot, t);
-	shape.setRotation(rot);
+	//float rot = lerp(startRot, endRot, t);
+	//shape.setRotation(rot);
 
 	window.draw(shape);
 }
